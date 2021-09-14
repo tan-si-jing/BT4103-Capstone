d0efa0db65516b96abe41389e8d0c058d4d53ec7
tan-si-jing <65453969+tan-si-jing@users.noreply.github.com>
2021-09-15 04:37:39 +0800

    Added routes and edited AdminLogin

---
+++ src/components/UserTypeButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <router-link to="/login">
+    <button type="button" class="btn btn-outline-dark" style="button">
+      <i :class="['bi-' + icon]" style="font-size: 4rem"></i>
+    </button>
+    </router-link>
+    <h6>{{ label }}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserTypeButton',
+  props: {
+    label: String,
+    icon: String
+  }
+}
+</script>
+
+<style scoped>
+button {
+  height: 9rem;
+  width: 9rem;
+  border-radius: 1.5rem;
+  background-color: white;
+  box-shadow: 2px 2px rgba(0, 0, 0, 0.2);
+  border-color: transparent;
+}
+button:hover {
+  background-color: #40AAAD;
+  color: white;
+  border-color: transparent;
+}
+h6 {
+  padding-top: 0.5rem;
+  text-align:center
+}
+</style>
