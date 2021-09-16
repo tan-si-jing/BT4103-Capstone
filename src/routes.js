8e78bce7cc2355305b0588f59e76df27919a20ce
Mei Jun <chmeijun2@gmail.com>
2021-09-16 13:25:32 +0800

    html and css for Results page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { createRouter,createWebHistory } from 'vue-router'
 import Home from "./screens/Home.vue";
-import Login from "./screens/AdminLogin.vue"
+import Login from "./screens/AdminLogin.vue";
+//import Results from "./screens/Results.vue"
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,6 +14,12 @@ const router = createRouter({
       path: "/login",
       component: Login
     },
+    /*
+    {
+      path:"/results",
+      components: Results
+    },*/
+
     /*{
       path: "/search",
       component: Search,
