4903aba07ecefe0d771a79c7fee065cb2ace0eac
lucastan17 <48749186+lucastan17@users.noreply.github.com>
2021-09-23 19:24:45 +0800

    Finish Level 2 Search Parameters and compiled into results page
    Finished the remaining 7 level 2 search parameters and appended the results tables into a Level 2 Results page.

---
+++ src/screens/LateralClearance.vue
@@ -0,0 +1,148 @@
+<template>
+      <table class="table table-borderless">
+        <thead class="table">
+          <tr>
+          <td scope="header">9 &nbsp; Lateral Clearance</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <td scope="row">
+            <div class="content">
+                <h2>10.7.1 Lateral Clearance</h2>
+                <h3 style="padding-top:10px"><b>10.7.1.1</b></h3>
+                    <p>Minimum lateral clearance from the edge of road pavement (both at centre median and side table) to any adjacent 
+                        structure is shown in Table 10.17 unless otherwise specified in the Standard Details of Road Elements.</p>
+                    <div class="img-container">
+                        <img src="../assets/Table10-17.png">
+                    </div>
+                    <br>Notes:
+                    <p>1 If no Vehicular Impact Guardrail is installed. Applicable to both centre median and sidetable.</p>
+                    <p>2 Where Vehicular Impact Guardrail is installed.</p>
+
+                <h3 style="padding-top:10px"><b>10.7.1.2</b></h3>
+                    <p>For an elevated road structure, the minimum lateral clearance between the edge of the road structure and any other structures shall be 3m.</p>
+
+                <h3 style="padding-top:10px"><b>10.5.11.5</b></h3>
+                    <p>Directional sign shall generally be sited on the side next to the slow lane and clearly visible to the driver. It shall be 
+                        noted that in some cases (e.g.for a major right turning movement, or for very wide road) it may be appropriate to site sign 
+                        on the median if it is wide enough. The minimum lateral clearance from the edge of the vehicular impact guardrail to the 
+                        edge of the signboard shall be at least 600mm. The post/support of the signboard shall be at least 1000mm from the vehicular 
+                        impact guardrail. Where there is no vehicular impact guardrail next to the raise kerb, the minimum clearance between the edge of 
+                        signboard and the edge of road kerb shall be at least 600mm.</p> 
+            </div>
+          </td>
+          </tr>
+          <tr>
+          <td scope="row"></td>
+          </tr>
+          <tr>
+          <td scope="row">
+          <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+            <div class="grid-container">
+              Reference from:
+              <div class="grid-item inner-grid-container">
+                <div class="grid-item">
+                  <span id = "manuals"> Manuals </span>
+                  <br>Civil Design Criteria (CDC)
+                </div>   
+                
+                <div class="grid-item">
+                  <span id = "chapter"> Chapter/ Section </span>
+                  <br>CDC Chapter 10 / Section 10.7.1.1, 10.7.1.2, 10.5.11.5
+                </div>   
+                
+                <div class="grid-item">
+                  <span id = "link">Link to Chapter/ Section</span>
+                  <br>
+                   <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/civil_standards/pdf/EGD09106A2_Overall.pdf#page=222">CDC 10.7.1.1 (page 222)</a>
+                  <br>
+                   <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/civil_standards/pdf/EGD09106A2_Overall.pdf#page=223">CDC 10.7.1.2 (page 223)</a>
+                  <br>
+                   <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/civil_standards/pdf/EGD09106A2_Overall.pdf#page=220">CDC 10.5.11.5 (page 220)</a>
+                </div>   
+              </div>
+            </div>      
+          </td>
+          </tr>
+        </tbody>
+      </table>
+</template>
+
+<style scoped>
+table {
+    width: 70vw;
+    height: 60vh;
+    margin-top: 5vh;
+    border-radius: 15px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+    z-index: 999;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: auto;
+}
+.table thead td {
+    background-color: #273B8C;
+    color: white;
+    height: 6vh;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    font-size:22px;
+    text-indent:1.5%;
+}
+tbody tr{
+    background-color: #ffffff;
+}
+
+tbody tr:last-child td{ 
+    border-bottom-right-radius: 15px;
+    border-bottom-left-radius: 15px; 
+    height: 26vh;
+}
+
+.content{
+  font-size:18px;
+  margin: 2% 10%;
+  text-align: justify;
+}
+
+.grid-container{
+  margin:5% 5% -8%;
+  font-size:20px;
+}
+
+.grid-item {
+    padding: 10px;
+    font-size:16px;
+}
+
+.grid-container .inner-grid-container {
+	display: grid;
+	grid-template-columns: auto auto auto;
+	text-align: center;
+}
+
+#manuals{
+  font-weight:bold;
+}
+
+#chapter{
+  font-weight:bold;
+}
+
+#link{
+  font-weight:bold;
+}
+
+.img-container{
+    width: 500px;
+    height: 300px;
+}
+
+/* Resize images to standardise size */
+.img-container img{
+    max-width: 100%;
+    height: auto;
+}
+</style>
+
---
+++ src/screens/Level2Results.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="wrapper">
+    <p class="header">Results</p>
+    <div class="container">
+      <RoadCSElem></RoadCSElem>
+    </div>
+    <div class="container">
+      <Grade></Grade>
+    </div>
+    <div class="container">
+      <LongFrictFactor></LongFrictFactor>
+    </div>
+    <div class="container">
+      <SideFrictFactor></SideFrictFactor>
+    </div>
+    <div class="container">
+      <Crossfall></Crossfall>
+    </div>
+    <div class="container">
+      <CornerRadius></CornerRadius>
+    </div>
+    <div class="container">
+      <MergingAngle></MergingAngle>
+    </div>
+    <div class="container">
+      <LaneWidth></LaneWidth>
+    </div>
+    <div class="container">
+      <LateralClearance></LateralClearance>
+    </div>
+
+  <img :src="road" class="road" />
+  <img :src="mascot" class="mascot" />
+</div>
+</template>
+
+<script>
+import RoadCSElem from "./RoadCross-SectionsElements.vue"
+import Grade from "./Grade.vue"
+import LongFrictFactor from "./LongitudinalFrictionFactor.vue"
+import SideFrictFactor from "./SideFrictionFactor.vue"
+import Crossfall from "./Crossfall.vue"
+import CornerRadius from "./CornerRadius.vue"
+import MergingAngle from "./MergingAngle.vue"
+import LaneWidth from "./LaneWidth.vue"
+import LateralClearance from "./LateralClearance.vue"
+
+export default {
+name: "Level2Results",
+components: {
+  "RoadCSElem": RoadCSElem,
+  "Grade": Grade,
+  "LongFrictFactor": LongFrictFactor,
+  "SideFrictFactor": SideFrictFactor,
+  "Crossfall": Crossfall,
+  "CornerRadius": CornerRadius,
+  "MergingAngle": MergingAngle,
+  "LaneWidth": LaneWidth,
+  "LateralClearance": LateralClearance
+},
+props: {},
+data() {
+  return {
+    road: require("../assets/road.png"),
+    mascot: require("../assets/mascot.png"),
+  };
+},
+};
+</script>
+
+<style scoped>
+@import url(https://fonts.googleapis.com/css?family=Roboto);
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+}
+.wrapper {
+  background: #e0e0e0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+}
+.header {
+  font-size: 5vh;
+  margin: 2% 60% 0% 0%;
+  text-align:center
+}
+.container {
+  padding: 3% 11% 10%;
+}
+
+
+
+/** road image */
+.road {
+  max-width: 100%;
+  position: relative;
+  bottom: 0;
+  z-index: 1;
+}
+/** lta mascot */
+.mascot {
+  max-width: 15%;
+  position: absolute;
+  right: 4%;
+  bottom: 0.5%;
+  transform: rotateY(180deg);
+  z-index: 2;
+}
+</style>
