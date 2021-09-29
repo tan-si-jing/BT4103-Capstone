caeb3b79a93e7e0923bacd716a6df85c1bd1f0e3
Mei Jun <chmeijun2@gmail.com>
2021-09-29 18:56:19 +0800

    Added SDRE for Signs.vue

---
+++ src/screens/Signs.vue
@@ -0,0 +1,326 @@
+<template>
+      <table class="table table-borderless">
+        <thead class="table">
+          <tr>
+          <td scope="header">9 &nbsp; Signs</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <td scope="row">
+            <div class="content">
+                <p style="padding-top:3%"><b class="tab2">9.1</b><b>Directional Sign</b></p>
+                    <div class="img-container4">
+                        <img src="../assets/7-1.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=2" target="_blank">
+                  SDRE Chapter 7 - 1 (page 2)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/7-2.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=3" target="_blank">
+                  SDRE Chapter 7 - 2 (page 3)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/7-3.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=4" target="_blank">
+                  SDRE Chapter 7 - 3 (page 4)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/7-4.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=5" target="_blank">
+                  SDRE Chapter 7 - 4 (page 5)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/7-5.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=6" target="_blank">
+                  SDRE Chapter 7 - 5 (page 6)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/7-6.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=7" target="_blank">
+                  SDRE Chapter 7 - 6 (page 7)</a></p>
+                
+                    <div class="img-container4">
+                        <img src="../assets/7-7.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=8" target="_blank">
+                  SDRE Chapter 7 - 7 (page 8)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/7-8.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=9" target="_blank">
+                  SDRE Chapter 7 - 8 (page 9)</a></p>
+
+                
+
+                <p style="padding-top:3%"><b class="tab2">9.2</b><b>Traffic Mandatory Signs</b></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/15-1.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-15_TFM_1-2-JULY_2020.pdf#page=2" target="_blank">
+                  SDRE Chapter 15 - 1 (page 2)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/15-2.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-15_TFM_1-2-JULY_2020.pdf#page=3" target="_blank">
+                  SDRE Chapter 15 - 2 (page 3)</a></p>
+
+
+
+                <p style="padding-top:3%"><b class="tab2">9.3</b><b>Traffic Prohibitory Signs</b></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/16-1.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-16_TFP_1-6-JULY_2020.pdf#page=2" target="_blank">
+                  SDRE Chapter 16 - 1 (page 2)</a></p>
+                
+                    <div class="img-container4">
+                        <img src="../assets/16-2.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-16_TFP_1-6-JULY_2020.pdf#page=3" target="_blank">
+                  SDRE Chapter 16 - 2 (page 3)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/16-3.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-16_TFP_1-6-JULY_2020.pdf#page=4" target="_blank">
+                  SDRE Chapter 16 - 3 (page 4)</a></p>
+                
+                    <div class="img-container4">
+                        <img src="../assets/16-4.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-16_TFP_1-6-JULY_2020.pdf#page=5" target="_blank">
+                  SDRE Chapter 16 - 4 (page 5)</a></p>
+                
+                    <div class="img-container4">
+                        <img src="../assets/16-5.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-16_TFP_1-6-JULY_2020.pdf#page=6" target="_blank">
+                  SDRE Chapter 16 - 5 (page 6)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/16-6.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-16_TFP_1-6-JULY_2020.pdf#page=7" target="_blank">
+                  SDRE Chapter 16 - 6 (page 7)</a></p>
+
+
+                <p style="padding-top:3%"><b class="tab2">9.4</b><b>Traffic Warning Signs</b></p>
+                    <div class="img-container4">
+                        <img src="../assets/17-1.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-17_TFW_1-9-JULY_2020.pdf#page=2" target="_blank">
+                  SDRE Chapter 17 - 1 (page 2)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/17-2.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-17_TFW_1-9-JULY_2020.pdff#page=3" target="_blank">
+                  SDRE Chapter 17 - 2 (page 3)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/17-3.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-17_TFW_1-9-JULY_2020.pdf#page=4" target="_blank">
+                  SDRE Chapter 17 - 3 (page 4)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/17-4.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-17_TFW_1-9-JULY_2020.pdf#page=5" target="_blank">
+                  SDRE Chapter 17 - 4 (page 5)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/17-5.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-17_TFW_1-9-JULY_2020.pdf#page=6" target="_blank">
+                  SDRE Chapter 17 - 5 (page 6)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/17-6.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-17_TFW_1-9-JULY_2020.pdf#page=7" target="_blank">
+                  SDRE Chapter 17 - 6 (page 7)</a></p>
+                
+                    <div class="img-container4">
+                        <img src="../assets/17-7.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-17_TFW_1-9-JULY_2020.pdf#page=8" target="_blank">
+                  SDRE Chapter 17 - 7 (page 8)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/17-8.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-17_TFW_1-9-JULY_2020.pdf#page=9" target="_blank">
+                  SDRE Chapter 17 - 8 (page 9)</a></p>
+
+
+
+                <p style="padding-top:3%"><b class="tab2">9.5</b><b>Traffic Informatory Signs</b></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/18-1.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=2" target="_blank">
+                  SDRE Chapter 18 - 1 (page 2)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/18-2.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=3" target="_blank">
+                  SDRE Chapter 18 - 2 (page 3)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/18-3.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=4" target="_blank">
+                  SDRE Chapter 18 - 3 (page 4)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/18-4.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=5" target="_blank">
+                  SDRE Chapter 18 - 4 (page 5)</a></p>
+
+                  <div class="img-container4">
+                        <img src="../assets/18-5.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=6" target="_blank">
+                  SDRE Chapter 18 - 5 (page 6)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/18-6.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=7" target="_blank">
+                  SDRE Chapter 18 - 6 (page 7)</a></p>
+                
+                    <div class="img-container4">
+                        <img src="../assets/18-7.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=8" target="_blank">
+                  SDRE Chapter 18 - 7 (page 8)</a></p>
+
+                    <div class="img-container4">
+                        <img src="../assets/18-8.png">
+                    </div>
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/Street_Work_Proposals/Standards_and_Specifications/SDRE/SDRE14-7_DIR_1-8-JULY_2020.pdf#page=9" target="_blank">
+                  SDRE Chapter 18 - 8 (page 9)</a></p>
+
+
+                  
+
+                <p style="padding-top:3%"><b class="tab2">9.6</b><b>Street Name Board</b></p>
+
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/civil_standards/pdf/EGD09106A2_Overall.pdf#page=201" target="_blank">
+                  CDC 10.4.1.3 - Combination of Horizontal & Vertical Alignment (page 201)</a></p>
+
+                <p style="padding-top:3%"><b class="tab2">9.7</b><b>Signs – Cycling Path</b></p>
+
+                <p style="font-size: 16px;"> Referenced from:
+                  <a href="https://www.lta.gov.sg/content/dam/ltagov/industry_innovations/industry_matters/development_construction_resources/civil_standards/pdf/EGD09106A2_Overall.pdf#page=201" target="_blank">
+                  CDC 10.4.1.3 - Combination of Horizontal & Vertical Alignment (page 201)</a></p>
+
+            </div>
+
+          </td>
+          </tr>
+        </tbody>
+      </table>
+</template>
+
+<style scoped>
+table {
+    width: 70vw;
+    height: auto;
+    margin-top: 5vh;
+    border-radius: 15px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+    z-index: 999;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: auto;
+}
+.table thead td {
+    background-color: #273B8C;
+    color: white;
+    height: 6vh;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    font-size:22px;
+    text-indent:1.5%;
+    vertical-align: middle;
+}
+tbody tr{
+    background-color: #ffffff;
+}
+
+tbody tr:last-child td{ 
+    border-bottom-right-radius: 15px;
+    border-bottom-left-radius: 15px; 
+    height: 31vh;
+}
+
+.content{
+  font-size:18px;
+  margin: 2% 10%;
+  text-align: justify;
+  padding-bottom:5%;
+}
+p {
+  font-size:20px;
+}
+.tab1 {
+ margin-right: 2.5em
+}
+
+.tab2 {
+ margin-right: 1.7em
+}
+
+.tab3 {
+  margin-right: 1em
+}
+</style>
