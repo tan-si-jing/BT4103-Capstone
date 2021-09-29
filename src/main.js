faabf7153dd532d4ae779683cb0194ce8eeea2c7
lee-tricia <80332823+lee-tricia@users.noreply.github.com>
2021-09-29 22:32:49 +0800

    Add collapse

--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import router from "./routes";
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faAngleDown, faAngleUp)
+
 const app = createApp({
         render: () =>h(App)
     })
 
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
