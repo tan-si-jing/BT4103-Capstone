6998b9569f80ab3838995ad76c94963e4fbb4488
Mei Jun <chmeijun2@gmail.com>
2021-10-07 12:14:40 +0800

    Updated routes

--- src/screens/Results2.vue
+++ src/screens/Results2.vue
@@ -86,6 +86,9 @@ methods: {
   level5() {
     this.$router.push({path: "/results/page4"})
   },
+    back() {
+    this.$router.push({path: "/search5"})
+  },
   openStorage(){
         return JSON.parse(localStorage.getItem('choice'))
   },
