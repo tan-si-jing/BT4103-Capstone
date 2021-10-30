c4ddf17497ae145b331b2be32dbe6f687817780c
tan-si-jing <tan-si-jing@users.noreply.github.com>
2021-10-31 03:40:48 +0800

    add zoom image component

---
+++ src/components/ZoomImage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img @click="show" :src="require(`${imgSrc}`)">
+
+    <vue-easy-lightbox
+      :visible="visible"
+      :imgs="imgSrc"
+      @hide="handleHide"
+    ></vue-easy-lightbox>
+  </div>
+</template>
+
+<script>
+//import VueEasyLightbox from 'vue-easy-lightbox'
+
+export default {
+  components: {
+    VueEasyLightbox
+  },
+  props:{
+    imgSrc: String
+  },
+  data() {
+    return {
+      visible: false,
+    }
+  },
+  methods: {
+    show() {
+      this.visible = true
+    },
+    handleHide() {
+      this.visible = false
+    }
+  }
+}
+</script>
